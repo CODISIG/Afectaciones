011428b75c3a591f5694734312d3a7d6fdb2021a
CODISIG <codisig25@gmail.com>
2025-04-10 17:27:30 -0600

    Afectaciones

---
+++ Afect_Geojson/etiqueta.js
@@ -0,0 +1,6 @@
+var etiqueta = {"type": "FeatureCollection","features": [
+{ "type": "Feature", "properties": { "id": 1 , "nom_nuc": "SAN PEDRO TLAQUILPAN" }, "geometry": { "type": "Point", "coordinates": [ -98.803070459243628, 19.963049490192919 ] } },
+{ "type": "Feature", "properties": { "id": 2 , "nom_nuc": "ACAYUCA" }, "geometry": { "type": "Point", "coordinates": [ -98.79820251891579, 19.991420887708404 ] } },
+{ "type": "Feature", "properties": { "id": 3 , "nom_nuc": "PACHUQUILLA" }, "geometry": { "type": "Point", "coordinates": [ -98.75058664783911, 20.010076239893348  ] } }
+]
+}
