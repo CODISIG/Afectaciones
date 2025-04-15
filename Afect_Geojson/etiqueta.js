8937ed73a99ea8636e27872353ed9e9bfe97390f
CODISIG <codisig25@gmail.com>
2025-04-15 16:26:58 -0600

    Afectaciones

---
+++ Afect_Geojson/etiqueta.js
@@ -0,0 +1,11 @@
+var etiqueta = {"type": "FeatureCollection","features": [
+{ "type": "Feature", "properties": { "id": 1 , "nom_nuc": "SAN PEDRO TLAQUILPAN" }, "geometry": { "type": "Point", "coordinates": [ -98.803070459243628, 19.963049490192919 ] } },
+{ "type": "Feature", "properties": { "id": 2 , "nom_nuc": "ACAYUCA" }, "geometry": { "type": "Point", "coordinates": [ -98.79820251891579, 19.991420887708404 ] } },
+{ "type": "Feature", "properties": { "id": 3 , "nom_nuc": "PACHUQUILLA" }, "geometry": { "type": "Point", "coordinates": [ -98.75058664783911, 20.010076239893348  ] } },
+{ "type": "Feature", "properties": { "id": 4 , "nom_nuc": "JAGUEY DE TELLEZ" }, "geometry": { "type": "Point", "coordinates": [ -98.78094434187071, 20.00538378222968  ] } },
+{ "type": "Feature", "properties": { "id": 5 , "nom_nuc": "TEMASCALAPA" }, "geometry": { "type": "Point", "coordinates": [ -98.8926281620881, 19.855735830311218  ] } },
+{ "type": "Feature", "properties": { "id": 6 , "nom_nuc": "SAN MATEO IXTLAHUACA" }, "geometry": { "type": "Point", "coordinates": [ -98.8821628274403, 19.895103656114472 ] } }
+]
+}
+
+
