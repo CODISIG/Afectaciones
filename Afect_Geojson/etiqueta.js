6de33b3a261d2896833a3938137b95a1c862fb12
CODISIG <codisig25@gmail.com>
2025-05-15 17:55:19 -0600

    AFECTACIONES

---
+++ Afect_Geojson/etiqueta.js
@@ -0,0 +1,15 @@
+var etiqueta = {"type": "FeatureCollection","features": [
+{ "type": "Feature", "properties": { "id": 1 , "nom_nuc": "SAN PEDRO TLAQUILPAN" }, "geometry": { "type": "Point", "coordinates": [ -98.803070459243628, 19.963049490192919 ] } },
+{ "type": "Feature", "properties": { "id": 2 , "nom_nuc": "ACAYUCA" }, "geometry": { "type": "Point", "coordinates": [ -98.79820251891579, 19.991420887708404 ] } },
+{ "type": "Feature", "properties": { "id": 3 , "nom_nuc": "PACHUQUILLA" }, "geometry": { "type": "Point", "coordinates": [ -98.75058664783911, 20.010076239893348  ] } },
+{ "type": "Feature", "properties": { "id": 4 , "nom_nuc": "JAGUEY DE TELLEZ" }, "geometry": { "type": "Point", "coordinates": [ -98.78094434187071, 20.00538378222968  ] } },
+{ "type": "Feature", "properties": { "id": 5 , "nom_nuc": "TEMASCALAPA" }, "geometry": { "type": "Point", "coordinates": [ -98.8926281620881, 19.855735830311218  ] } },
+{ "type": "Feature", "properties": { "id": 6 , "nom_nuc": "SAN MATEO IXTLAHUACA" }, "geometry": { "type": "Point", "coordinates": [ -98.8821628274403, 19.895103656114472 ] } },
+{ "type": "Feature", "properties": { "id": 7 , "nom_nuc": "SAN ILDEFONSO" }, "geometry": { "type": "Point", "coordinates": [ -99.28645, 19.91630  ] } },
+{ "type": "Feature", "properties": { "id": 8 , "nom_nuc": "SAN CLEMENTE" }, "geometry": { "type": "Point", "coordinates": [ -100.08296, 20.48966  ] } },
+{ "type": "Feature", "properties": { "id": 8 , "nom_nuc": "SAN CLEMENTE" }, "geometry": { "type": "Point", "coordinates": [ -100.09571, 20.49742  ] } },
+{ "type": "Feature", "properties": { "id": 9 , "nom_nuc": "PEDRO ESCOBEDO" }, "geometry": { "type": "Point", "coordinates": [ -100.11643, 20.51506  ] } },
+{ "type": "Feature", "properties": { "id": 10 , "nom_nuc": "AGUA_AZUL" }, "geometry": { "type": "Point", "coordinates": [ -100.19759, 20.60125  ] } },
+{ "type": "Feature", "properties": { "id": 11 , "nom_nuc": "EL MUERTO IGNACIO PÉREZ" }, "geometry": { "type": "Point", "coordinates": [ -100.08897, 20.47452 ] } }
+]
+}
